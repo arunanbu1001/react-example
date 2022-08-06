9a7b08db34bbd7fb4eab523797ec5ec7dbdb2913
arunanbu1001 <arunanbu10012001@gmail.com>
2022-08-06 21:43:48 +0530

    add

---
+++ Arun.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import example from './example'
+
+function Arun() {
+  return (
+    <div>
+        <example />
+    </div>
+  )
+}
+
+export default Arun
--- export.js
+++ export.js
@@ -1,8 +1,32 @@
 export const product=[
     {
-        id :1;
-        name : "cart1";
-        description : "adhgjkvbvfd dsuhujdhVIUFHvufhbvf dsh"; 
+        id :1,
+        name :"cart1",
+        description : "adhgjkvbvfd dsuhujdhVIUFHvufhbvf dsh",
+    
+    },
+    {
+        id :1,
+        name :"cart1",
+        description : "adhgjkvbvfd dsuhujdhVIUFHvufhbvf dsh",
+    
+    },
+    {
+        id :1,
+        name :"cart1",
+        description : "adhgjkvbvfd dsuhujdhVIUFHvufhbvf dsh",
+    
+    },
+    {
+        id :1,
+        name :"cart1",
+        description : "adhgjkvbvfd dsuhujdhVIUFHvufhbvf dsh",
+    
+    },
+    {
+        id :1,
+        name :"cart1",
+        description : "adhgjkvbvfd dsuhujdhVIUFHvufhbvf dsh",
     
     }
 ]
